2f6f7b0aaf64bf8229af70555fc62b8b93389f86
BarackOBusiness <nichjoh21@gmail.com>
2024-05-10 21:14:31 -0400

    Initial project skeleton

---
+++ Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+
+namespace TheLagFixer;
+
+[BepInPlugin("GOI.Core.LagFixer", "The Lag Fixer", "0.1.0")]
+public class Plugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger.LogInfo($"The lag fixer has loaded.");
+    }
+}
