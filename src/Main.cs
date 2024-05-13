5bc00559a5791016a79220039b221b87eacb203f
BarackOBusiness <nichjoh21@gmail.com>
2024-05-13 15:42:45 -0400

    Setup building and project layout

--- src/Main.cs
+++ src/Main.cs
@@ -1,13 +1,41 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace TheLagFixer;
 
-[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-public class Plugin : BaseUnityPlugin
+[BepInPlugin("goi.core.lagfixer", "The Lag Fixer", "0.1.0")]
+public class TheLagFixer : BaseUnityPlugin
 {
+    public ConfigEntry<bool> alternate;
+    public ConfigEntry<bool> smooth;
+    
     private void Awake()
     {
+        alternate = Config.Bind(
+            "",
+            "Use alternative camera follow logic?",
+            false,
+            "With this option on, the camera gains a rigidbody that is moved on physics updates and interpolated like the player, with it off, the camera moves in LateUpdate"
+        );
+
+        smooth = Config.Bind(
+            "",
+            "Smoothed timestep",
+            false,
+            "Whether or not to use a smoothed value for multiplying the camera velocity every frame, only applies with alternate camera logic turned off."
+        );
+    
         // Plugin startup logic
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        Logger.LogInfo($"All forms of physics engine induced stutter may have been fixed!");
+        SceneManager.sceneLoaded += OnSceneLoad;
     }
+
+    private void OnSceneLoad(Scene scene, LoadSceneMode mode) {
+        
+    }
+}
+
+public class CameraControlAssist : MonoBehaviour {
 }
